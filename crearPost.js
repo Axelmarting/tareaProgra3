5637ddec9f6d0c440ff5b3daf6f2042da854f7db
Axelmarting <axelmarting2002@gmail.com>
2026-04-19 21:35:55 -0300

    publicacion interactiva

--- crearPost.js
+++ crearPost.js
@@ -1,11 +1,17 @@
 function crearPost(){
     const feed = document.getElementById("feed");
+    const input = document.getElementById("inputMedio");
+
+    if (input.value.trim() === "") {
+        alert("Input vacio, escribir algo para publicar");
+        return;
+    }
 
     const div = document.createElement("div");
     div.classList.add("post");
 
     const texto = document.createElement("p");
-    texto.innerText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque impedit, id velit provident eligendi tempore dolorem voluptatem quo dolorum perspiciatis!";
+    texto.innerText = input.value;
 
     feed.appendChild(div);
     div.appendChild(texto);
